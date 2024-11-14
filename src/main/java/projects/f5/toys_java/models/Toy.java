04cba967bfe4ba4db699909ca7ada9c8c5061b81
tizzifona <tizzifona@gmail.com>
2024-11-14 14:52:37 +0100

    Added model Toy.java

--- src/main/java/projects/f5/toys_java/models/Toy.java
+++ src/main/java/projects/f5/toys_java/models/Toy.java
@@ -1,5 +1,32 @@
 package projects.f5.toys_java.models;
 
 public class Toy {
+    protected String id;
+    protected String customId;
+    protected String title;
+    protected int childType;
+
+    public Toy(String id, String customId, String title, int childType) {
+        this.id = id;
+        this.customId = customId;
+        this.title = title;
+        this.childType = childType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCustomId() {
+        return customId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getChildType() {
+        return childType;
+    }
 
 }
