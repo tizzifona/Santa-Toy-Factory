ec580274ff0f3edb9dd32901598a689a534ce1ef
tizzifona <tizzifona@gmail.com>
2024-11-14 15:01:34 +0100

    Added BadToy.java

--- src/main/java/projects/f5/toys_java/models/BadToy.java
+++ src/main/java/projects/f5/toys_java/models/BadToy.java
@@ -1,5 +1,29 @@
 package projects.f5.toys_java.models;
 
-public class BadToy {
+public class BadToy extends Toy {
+    private String content;
+
+    public BadToy(String id, String customId, String title, String content) {
+        super(id, customId, title, 2);
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        String toyDetails = "❄️";
+        toyDetails += customId + ".\n";
+
+        String cyanColor = "\u001B[36m";
+        String resetColor = "\u001B[0m";
+
+        toyDetails += cyanColor + "Title: " + title + "\n" + resetColor;
+        toyDetails += cyanColor + "Content: " + content + "\n" + resetColor;
+
+        return toyDetails;
+    }
 
 }
