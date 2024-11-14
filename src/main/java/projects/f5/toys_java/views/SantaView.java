fb4080b2d84bbb2cadd3f9ce3a3c9d1bf3dfca94
tizzifona <tizzifona@gmail.com>
2024-11-14 15:54:37 +0100

    Added SantaView.java

--- src/main/java/projects/f5/toys_java/views/SantaView.java
+++ src/main/java/projects/f5/toys_java/views/SantaView.java
@@ -1,5 +1,18 @@
 package projects.f5.toys_java.views;
 
-public class SantaView {
+public class SantaView extends View {
+    public int showSantaMenu() {
+        System.out.println("\033[34m1. View Good Children Toys\033[0m");
+        System.out.println("\033[34m2. View Bad Children Toys\033[0m");
+        System.out.println("\033[34m3. Save All Toys to CSV\033[0m");
+        System.out.println("\033[31m4. Log Out\033[0m");
+        System.out.print("Select an option: ");
+        return scanner.nextInt();
+    }
 
+    public String getCsvFilename() {
+        scanner.nextLine();
+        System.out.print("Enter the filename to save: ");
+        return scanner.nextLine();
+    }
 }
