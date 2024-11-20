5e6b47e193120fa417091119ee2ca4abd90e0856
tizzifona <tizzifona@gmail.com>
2024-11-20 09:24:24 +0100

    Divided DatabaseConnection into DatabaseConnection and DatabaseManipulation

--- src/main/java/projects/f5/toys_java/bd/DatabaseConnection.java
+++ src/main/java/projects/f5/toys_java/bd/DatabaseConnection.java
@@ -3,7 +3,6 @@ package projects.f5.toys_java.bd;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DatabaseConnection {
 
@@ -27,26 +26,6 @@ public class DatabaseConnection {
             return connection != null && !connection.isClosed();
         } catch (SQLException e) {
             return false;
-        }
-    }
-
-    public static void createTableIfNotExists() {
-        String sql = "CREATE TABLE IF NOT EXISTS toys (" +
-                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "custom_id TEXT UNIQUE, " +
-                "title TEXT NOT NULL, " +
-                "brand TEXT, " +
-                "recommended_age INTEGER, " +
-                "category TEXT, " +
-                "content TEXT, " +
-                "child_type INTEGER NOT NULL)";
-        Connection conn = connect();
-
-        try (Statement stmt = conn.createStatement()) {
-            stmt.execute(sql);
-        } catch (SQLException e) {
-            System.out.println("Error creating the table.");
-            e.printStackTrace();
         }
     }
 
