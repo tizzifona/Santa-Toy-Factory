4e5263ceddd813f81100cfec4feab3af49483018
tizzifona <tizzifona@gmail.com>
2024-11-20 15:49:12 +0100

    Added test for View

--- src/test/java/projects/f5/toys_java/views/ViewTest.java
+++ src/test/java/projects/f5/toys_java/views/ViewTest.java
@@ -1,4 +1,19 @@
 package projects.f5.toys_java.views;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Scanner;
+import org.junit.jupiter.api.Test;
+
 public class ViewTest {
+    @Test
+    void testCloseScanner() {
+        View mockView = new View() {
+        };
+        mockView.scanner = new Scanner(System.in);
+        mockView.closeScanner();
+
+        assertThrows(IllegalStateException.class, () -> {
+            mockView.scanner.nextLine();
+        });
+    }
 }
